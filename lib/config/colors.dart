ec27fe49364423bff12350bd26bc37512158b82e
Lucas Reis <lucasreis_9@hotmail.com>
2023-05-12 11:56:27 -0300

    Add imagens, icons, colors and dependecies

---
+++ lib/config/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColo = Color(0XFF1FCC79);
+const kSecondaryColor = Color(0XFFFF6464);
+const kMainTextColor = Color(0XFF2E3E5C);
+const kSecondaryTextColor = Color(0XFF9FA5C0);
+const kOutlineColor = Color(0XFFD0DBEA);
+const kFormColor = Color(0XFF4F5F7);
